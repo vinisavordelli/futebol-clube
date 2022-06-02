76a3416d8b33e661e26f42167641450b178bfad9
Vinicius <vinicius.savordelli@gmail.com>
2022-06-02 17:29:17 -0300

    adds login to user controller

--- app/backend/src/controllers/User.ts
+++ app/backend/src/controllers/User.ts
@@ -1,4 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import UserService from 'src/services/User';
-import ILogin from '../interfaces/ILogin';
+import UserService from '../services/User';
+
+export default class UserController {
+  static async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body;
+      const result = await UserService.Login({ email, password });
+
+      if (!result) {
+        return next(({ type: 'UNAUTHORIZED', message: 'Incorrect email or password' }));
+      }
+
+      return res.status(StatusCodes.OK).json({ ...result });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
