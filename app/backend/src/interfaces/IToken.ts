dab6dd8dfe33d9f9043f5fb42465141b4e3211d7
Vinicius <vinicius.savordelli@gmail.com>
2022-06-02 16:10:36 -0300

    modifies IUser interface and adds IToken interface

---
+++ app/backend/src/interfaces/IToken.ts
@@ -0,0 +1,6 @@
+import IUser from './IUser';
+
+export default interface IToken {
+  user: IUser,
+  token: string,
+}
--- app/backend/src/interfaces/IUser.ts
+++ app/backend/src/interfaces/IUser.ts
@@ -1,10 +1,6 @@
 export default interface IUser {
-  user: {
-    id: number;
-    username: string;
-    role: string;
-    email?: string,
-    password?: string,
-  },
-  token: string
+  id ?: number;
+  username: string;
+  role: string;
+  email: string;
 }
