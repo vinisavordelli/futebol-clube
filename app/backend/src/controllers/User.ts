7b21ca8e2ee80842ac620726b9d516d00c428f13
Vinicius <vinicius.savordelli@gmail.com>
2022-06-03 08:43:39 -0300

    fix some things

--- app/backend/src/controllers/User.ts
+++ app/backend/src/controllers/User.ts
@@ -3,16 +3,23 @@ import { StatusCodes } from 'http-status-codes';
 import UserService from '../services/User';
 
 export default class UserController {
-  static async login(req: Request, res: Response, next: NextFunction) {
+  static async login(req: Request, res: Response, _next: NextFunction) {
     try {
       const { email, password } = req.body;
+      if (!email || !password) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          message: 'All fields must be filled',
+        });
+      }
       const result = await UserService.Login({ email, password });
 
       if (!result) {
-        return next(({ type: 'UNAUTHORIZED', message: 'Incorrect email or password' }));
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+          message: 'Incorrect email or password',
+        });
       }
 
-      return res.status(StatusCodes.OK).json({ ...result });
+      return res.status(StatusCodes.OK).json(result);
     } catch (err) {
       console.log(err);
     }
