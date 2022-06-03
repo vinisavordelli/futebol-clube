43028cbe233992c92234225c56d82b85e490e870
Vinicius <vinicius.savordelli@gmail.com>
2022-06-03 16:54:34 -0300

    adiciona método getAll teams na controller

---
+++ app/backend/src/controllers/Team.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import TeamService from '../services/Team';
+
+export default class Team {
+  static async getAll(req:Request, res: Response, _next: NextFunction) {
+    try {
+      const result = await TeamService.getAll();
+      return res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
