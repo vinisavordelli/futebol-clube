7b375c3b74d7794e8110c673b7bc73403e8e4409
Vinicius <vinicius.savordelli@gmail.com>
2022-06-04 18:33:33 -0300

    adds create match service

---
+++ app/backend/src/interfaces/IError.ts
@@ -0,0 +1,3 @@
+export default interface IError {
+  err: string;
+}
--- app/backend/src/services/Match.ts
+++ app/backend/src/services/Match.ts
@@ -1,4 +1,6 @@
-import { ITeamsMatch } from '../interfaces/IMatch';
+import IError from 'src/interfaces/IError';
+import { StatusCodes } from 'http-status-codes';
+import { IMatch, ITeamsMatch } from '../interfaces/IMatch';
 import MatchModel from '../database/models/Match';
 import TeamModel from '../database/models/Team';
 
@@ -52,5 +54,25 @@ export default class Match {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  static async create(match: IMatch): Promise<ITeamsMatch | IError | string> {
+    if (match.awayTeam === match.homeTeam) {
+      return { status: StatusCodes.UNAUTHORIZED,
+        err: 'Teams cannot be the same' };
+    }
+    try {
+      const checkTeams = await Promise.all([
+        await TeamModel.findByPk(match.awayTeam),
+        await TeamModel.findByPk(match.homeTeam),
+      ]);
+      if (checkTeams.some((team) => team === null)) {
+        return {
+          status: StatusCodes.NOT_FOUND,
+          err: 'There is no team with such id!' };
+      }
+      const newMatch = await MatchModel.create(match);
+      return newMatch;
+    } catch (err) { console.log(err); }
   }
 }
