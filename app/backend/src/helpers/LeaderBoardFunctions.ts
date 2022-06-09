01d356ce88ec565857a3d6e87917777688fb1c3d
Vinicius <vinicius.savordelli@gmail.com>
2022-06-09 17:26:33 -0300

    refactor helper

--- app/backend/src/helpers/LeaderBoardFunctions.ts
+++ app/backend/src/helpers/LeaderBoardFunctions.ts
@@ -1,9 +1,33 @@
-export const calculateMatchPoints = (teamGoals:number, adversaryGoals:number):number => {
-  if (teamGoals === adversaryGoals) return 1;
-  if (teamGoals > adversaryGoals) return 3;
-  return 0;
+import { IMatch } from '../interfaces/IMatch';
+
+const IStats = {
+  totalGames: 0,
+  totalVictories: 0,
+  totalDraws: 0,
+  totalLosses: 0,
+  totalPoints: 0,
+  efficiency: 0,
+  goalsFavor: 0,
+  goalsOwn: 0,
 };
 
-export const calculateMatchEfficiency = (totalPoints:number, totalGames:number):number => Number(
-  ((totalPoints / (totalGames * 3)) * 100).toFixed(2),
-);
+const calculateTotals = (matches: IMatch[]) => {
+  const stats = { ...IStats };
+  matches.forEach((match) => {
+    if (match.homeTeamGoals > match.awayTeamGoals) {
+      stats.totalVictories += 1;
+      stats.totalPoints += 3;
+    }
+    if (match.homeTeamGoals === match.awayTeamGoals) {
+      stats.totalDraws += 1;
+      stats.totalPoints += 1;
+    }
+    if (match.awayTeamGoals < match.homeTeamGoals) { stats.totalLosses += 1; }
+    stats.goalsFavor += match.homeTeamGoals;
+    stats.goalsOwn += match.awayTeamGoals;
+  });
+  stats.efficiency = (stats.totalVictories * 3 + stats.totalDraws)
+   / (((stats.totalVictories + stats.totalDraws + stats.totalLosses) * 3) * 100);
+};
+
+export default calculateTotals;
